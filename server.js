27e3cdd3544f23c58d87377cb89c39731942edb4
Marcos-Cypriano <ma_cypriano@hotmail.com>
2020-03-28 09:39:44 -0300

    Back e front da aplicação

---
+++ server.js
@@ -0,0 +1,102 @@
+// Usar o Express para criar e configurar meu servidor
+const express = require('express')
+const server = express()
+
+const db = require("./db.js")
+
+// Configurar arquivos estaticos (css, scripts, imagens, etc)
+server.use(express.static("Public"))
+
+// Habilitar uso do req.body
+server.use(express.urlencoded({ extended: true }))
+
+// Configuração do nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure('views', {
+    express: server,
+    noCache: true, // variável boolean
+})
+
+
+// Criei um rota / e capturo o pedido do cliente para responder
+server.get("/", function(req, res) {
+    db.all(`SELECT * FROM ideas`, function (err, rows) {
+        if (err) {
+            console.log(err)
+            return res.send('Erro no banco de dados!')
+        }
+
+        const reversedIdeas = [...rows].reverse()
+        let lastIdeas = []
+
+        for (idea of reversedIdeas) {
+            if (lastIdeas.length < 2) {
+                lastIdeas.push(idea)
+            }
+        }
+
+        return res.render('index.html', { ideas: lastIdeas })
+    })
+})
+
+server.get("/ideias", function(req, res) {
+    db.all(`SELECT * FROM ideas`, function (err, rows) {
+        if (err) {
+            console.log(err)
+            return res.send('Erro no banco de dados!')
+        }
+
+        const reversedIdeas = [...rows].reverse()
+    
+        return res.render('ideias.html', { ideas: reversedIdeas })
+    })
+})
+
+server.post("/", function(req, res) {
+    const query = `
+    INSERT INTO ideas (
+        image,
+        title,
+        category,
+        description,
+        link
+    ) VALUES (?, ?, ?, ?, ?);
+    `
+
+    const values = [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link
+    ]
+
+    db.run(query, values, function(err) {
+        if (err) {
+            console.log(err)
+            return res.send('Erro no banco de dados!')
+        }
+
+        return res.redirect('/ideias')
+
+    })
+})
+
+server.delete("/:id", function (req, res) {
+
+    const ideaId = req.params.id
+
+    db.run(`DELETE FROM ideas WHERE id=?`, [ ideaId ], function(err) {
+        if (err) {
+            console.log(err)
+            return res.send('Não foi possível apagar a ideia.')
+        }
+
+        return res.send("OK")
+    })
+})
+
+
+
+// Liguei o servidor na porta local 3000
+server.listen(3000)
